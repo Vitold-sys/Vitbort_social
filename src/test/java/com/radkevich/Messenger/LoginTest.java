d66e7c34f0156590e2920df5cd979ae5ba090447
Vitold-sys <52232795+Vitold-sys@users.noreply.github.com>
2020-02-27 19:10:14 +0300

    Add JUnit

--- src/test/java/com/radkevich/Messenger/LoginTest.java
+++ src/test/java/com/radkevich/Messenger/LoginTest.java
@@ -1,11 +1,16 @@
 package com.radkevich.Messenger;
 
+import com.radkevich.Messenger.controller.AuthenticationController;
+import com.radkevich.Messenger.controller.UserController;
 import com.radkevich.Messenger.filter.CustomRsqlVisitor;
 import com.radkevich.Messenger.model.Message;
+import com.radkevich.Messenger.model.Role;
 import com.radkevich.Messenger.model.User;
 import com.radkevich.Messenger.repository.MessageRepo;
 import com.radkevich.Messenger.repository.UserRepository;
+import com.radkevich.Messenger.security.jwt.JwtTokenProvider;
 import com.radkevich.Messenger.service.MessageService;
+import com.radkevich.Messenger.service.UserService;
 import cz.jirutka.rsql.parser.RSQLParser;
 import cz.jirutka.rsql.parser.ast.Node;
 import org.junit.Test;
@@ -16,26 +21,48 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.parameters.P;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.radkevich.Messenger.model.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 
 public class LoginTest {
+
+    @Autowired
+    private  AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private UserService userService;
+
     @Autowired
     private MockMvc mockMvc;
+
+    private Set<Role> roles;
 
     @Autowired
     private MessageRepo messageRepo;
@@ -43,29 +70,50 @@ public class LoginTest {
     @Autowired
     private MessageService messageService;
 
+    @Autowired
+    private UserController userController;
+
+    @Autowired
+    private AuthenticationController authenticationController;
+
 
     @Test
     public void loginDeniedTest() throws Exception {
-        this.mockMvc.perform(get("/user"))
+        this.mockMvc.perform(get("/users"))
                 .andDo(print())
                 .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("http://localhost/login"));
+                .andExpect(redirectedUrl("http://localhost/8090/login"));
+    }
+
+
+    @Test
+    public void shouldNotAllowAccessToUnauthenticatedUsers() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/login")).andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void shouldGenerateAuthToken() throws Exception {
+            String username = "ad";
+        User user = userService.findByUsername(username);
+        String token = jwtTokenProvider.createToken("ad", user.getRoles());
+        assertNotNull(token);
+
     }
 
     @Test
     public void accessDeniedTest() throws Exception {
         this.mockMvc.perform(get("/messages"))
                 .andDo(print())
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("http://localhost/login"));
+                .andExpect(status().is4xxClientError());
     }
 
 
     @Test
-    public void badCredentials() throws Exception {
-        this.mockMvc.perform(post("/login").param("username", "jonh"))
+    public void AddMessage() throws Exception {
+        this.mockMvc.perform(post("/messages").param("message", "Loliz")
+                .param("tag", "Spring"))
                 .andDo(print())
-                .andExpect(status().isForbidden());
+                .andExpect(status().isOk());
     }
 
 
